ec49a9d9a1299f5132a0c2d92cc1a0a409c2591d
Jacob Sievers <jacosiev@gmail.com>
2025-02-06 09:44:43 +0100

    Fix text/background color contrast

--- ww-web/src/routes/Conversations.tsx
+++ ww-web/src/routes/Conversations.tsx
@@ -2,6 +2,29 @@ import MessagingContainer from "@/components/messaging/MessagingContainer";
 import { Box, chakra, VStack } from "@chakra-ui/react";
 import { Link, useParams } from "react-router";
 
+// from http://www.w3.org/TR/WCAG20/#relativeluminancedef
+function relativeLuminanceW3C(colorHex: string) {
+  const ar = colorHex.match(/../g) || [];
+
+  const [rNum, gNum, bNum] = ar.map((s) => parseInt(s, 16));
+
+  const RsRGB = rNum / 255;
+  const GsRGB = gNum / 255;
+  const BsRGB = bNum / 255;
+
+  const R =
+    RsRGB <= 0.03928 ? RsRGB / 12.92 : Math.pow((RsRGB + 0.055) / 1.055, 2.4);
+  const G =
+    GsRGB <= 0.03928 ? GsRGB / 12.92 : Math.pow((GsRGB + 0.055) / 1.055, 2.4);
+  const B =
+    BsRGB <= 0.03928 ? BsRGB / 12.92 : Math.pow((BsRGB + 0.055) / 1.055, 2.4);
+
+  // For the sRGB colorspace, the relative luminance of a color is defined as:
+  const L = 0.2126 * R + 0.7152 * G + 0.0722 * B;
+
+  return L;
+}
+
 function seededRandom(seed: number) {
   // Constants for LCG
   const a = 1103515245;
@@ -29,8 +52,11 @@ const ConversationLink = ({
   const colorNumber = Math.floor(
     seededRandom(parseInt(conversationId) || 1) * 16777215,
   );
-  const backgroundColor = "#" + colorNumber.toString(16);
-  const textColor = colorNumber > 0x800000 ? "white" : "gray.900";
+
+  const colorHexString = colorNumber.toString(16);
+  const backgroundColor = "#" + colorHexString;
+  const textColor =
+    relativeLuminanceW3C(colorHexString) < 0.5 ? "white" : "gray.900";
 
   console.log(isActive);
   return (
