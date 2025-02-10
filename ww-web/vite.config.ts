3f2c0de797bca983e04dc3b6cd89285d254e3a4f
Jacob Sievers <jacosiev@gmail.com>
2025-02-10 12:49:40 +0100

    Add ability to run www through docker

--- ww-web/vite.config.ts
+++ ww-web/vite.config.ts
@@ -4,5 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+    base: '/',
+    plugins: [react(), tsconfigPaths()],
 });
