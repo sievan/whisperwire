8ad9d61dafd405f7487eb3c77e808b3a78d3e45e
Jacob Sievers <jacosiev@gmail.com>
2025-02-11 15:11:40 +0100

    Add k8s setup

---
+++ infrastructure/create_k8s.sh
@@ -0,0 +1,7 @@
+kubectl apply -f k8s/persistent-volumes.yml
+kubectl apply -f k8s/persistent-volume-claims.yml
+kubectl apply -f k8s/zookeeper.yml
+kubectl apply -f k8s/kafka.yml
+kubectl apply -f k8s/postgres.yml
+kubectl apply -f k8s/api.yml
+kubectl apply -f k8s/www.yml
