d03d58bd709317f27bc0da1aea6accb213f46818
Jacob Sievers <jacosiev@gmail.com>
2025-02-14 10:05:24 +0100

    Add kafka topic initialization to k8s script

--- infrastructure/create_k8s.sh
+++ infrastructure/create_k8s.sh
@@ -2,6 +2,7 @@ kubectl apply -f k8s/persistent-volumes.yml
 kubectl apply -f k8s/persistent-volume-claims.yml
 kubectl apply -f k8s/zookeeper.yml
 kubectl apply -f k8s/kafka.yml
+kubectl apply -f k8s/jobs/kafka-topic-init.yml
 kubectl apply -f k8s/postgres.yml
 kubectl apply -f k8s/api.yml
 kubectl apply -f k8s/www.yml
